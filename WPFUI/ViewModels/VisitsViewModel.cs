b3ed5dbadd72bb46336b67e0875e416c87398d49
Michael Bent <Michaelobent@gmail.com>
2019-02-28 00:34:37 +0000

    Changed combobox source from list of activity model to string. Now reflecting changes 2 way properly

--- WPFUI/ViewModels/VisitsViewModel.cs
+++ WPFUI/ViewModels/VisitsViewModel.cs
@@ -19,16 +19,15 @@ namespace WPFUI.ViewModels
 		private VisitModel _selectedvisit = new VisitModel();
 		private MemberModel _selectedmember = new MemberModel();
 		private ObservableCollection<ActivityModel> _allActivityWithprices;
-		//private ObservableCollection<ActivityModel> _activitydetails;
-		private IEnumerable<string> _activitylist;
-		public ObservableCollection<ActivityModel> AllActivityPrices
-		{
-			get { return _allActivityWithprices; }
-			set { _allActivityWithprices = value; }
-		}
+		private ObservableCollection<string> _activitylist;
+		private ObservableCollection<string> _subactivitylist;
 
-		public IEnumerable<string> ActivityList
-		{ get
+		public IEnumerable<ActivityModel> AllActivityPrices;
+		
+
+		public ObservableCollection<string> ActivityList
+		{
+			get
 			{
 				if(_activitylist == null) return GetActivityList();
 				return _activitylist;
@@ -40,11 +39,20 @@ namespace WPFUI.ViewModels
 		}
 		
 
-		public ObservableCollection<ActivityModel> ActivityDetails
+		public ObservableCollection<string> SubActivityList
 		{
-			get { return GetActivityDetails(SelectedVisit.Activity.ActivityName, SelectedVisit.VisitDate); }
-			//set { OnPropertyChanged(ref _activitydetails, value); }
+			get
+			{
+				_subactivitylist = GetSubActivityList(SelectedVisit.Activity.ActivityName, SelectedVisit.VisitDate);
+				return _subactivitylist;
+			}
+				
+			set
+			{
+				OnPropertyChanged(ref _subactivitylist, value); 
+			}
 		}
+
 		public MemberModel SelectedMember
 		{
 			get { return _selectedmember; }
@@ -66,8 +74,8 @@ namespace WPFUI.ViewModels
 			get { return _selectedvisit; }
 			set
 			{
-				OnPropertyChanged(ref _selectedvisit, value);				
-				OnPropertyChanged("ActivityDetails");
+				OnPropertyChanged(ref _selectedvisit, value);
+				SubActivityList = null; //setting to any value requeries the list in get accessor through OnPropChanged, null is fine
 				SelectedMember = ShellViewModel.Softcache.Tables["Members"].GetMemberDetails(_selectedvisit.Member.MemNo);
 
 			}
@@ -79,20 +87,26 @@ namespace WPFUI.ViewModels
 			VisitConnector vconn = new VisitConnector();
 			Visits = new ObservableCollection<VisitModel>(vconn.Load("22/07/2017", true));
 			SelectedVisit = Visits.First<VisitModel>();
-			AllActivityPrices = new ObservableCollection<ActivityModel>(ShellViewModel.Softcache.Tables["Activities"].ToActivityModelIEnum());
+			AllActivityPrices = ShellViewModel.Softcache.Tables["Activities"].ToActivityModelIEnum();
 			
 		}
 		//move elsewhere :extension methods
-		private IEnumerable<string> GetActivityList()
-		{ return AllActivityPrices.Select(model => model.ActivityName).Distinct();}
+		private ObservableCollection<string> GetActivityList()
+		{ return new ObservableCollection<string>( AllActivityPrices.Select(model => model.ActivityName).Distinct());}
 
-		private ObservableCollection<ActivityModel> GetActivityDetails(string activityname, DateTime date)
+		private ObservableCollection<string> GetSubActivityList(string activityname, DateTime date)
 		{
-			return new ObservableCollection<ActivityModel>(AllActivityPrices.Where(model => 
-			model.ActivityName == activityname && model.IsWEBH == date.IsWeekendBankHoliday()));
+			return new ObservableCollection<string>(AllActivityPrices.Where(model =>
+			model.ActivityName == activityname && model.IsWEBH == date.IsWeekendBankHoliday()).Select(model => model.SubActivity));
+
+		}
+		private ObservableCollection<decimal> GetPrice(string activityname, DateTime date)
+		{
+			return new ObservableCollection<decimal>(AllActivityPrices.Where(model =>
+			model.ActivityName == activityname && model.IsWEBH == date.IsWeekendBankHoliday()).Select(model => model.Price));
 
 		}
 
-		
+
 	}
 }
